e3f2336f73002e077233173c140a9027b62af065
Cardan124 <carlosdanielvillenasantiago@MacBook-Pro-de-Carlos.local>
2022-02-21 19:27:08 -0600

    adding controllers for profesor entity

--- backend/controllers/profesores.js
+++ backend/controllers/profesores.js
@@ -3,8 +3,19 @@ const bcryptjs = require("bcryptjs");
 
 const { Profesor } = require("../models/profesores");
 
-const profesoresGet = (req = request, res = response) => {
-  res.status(200).json({ msg: "ok" });
+const profesoresGet = async (req = request, res = response) => {
+  const profesores = await Profesor.findAll();
+
+  res.json({ profesores });
+};
+
+const profesoresGets = async (req = request, res = response) => {
+  const { id } = req.params;
+
+  const profesor = await Profesor.findByPk(id);
+
+  if (profesor) res.json({ profesor });
+  else res.status(401).json({ msg: `No existe un profesor con el id ${id}` });
 };
 
 const profesoresPost = async (req = request, res = response) => {
@@ -29,12 +40,43 @@ const profesoresPost = async (req = request, res = response) => {
   } catch (err) {}
 };
 
-const profesoresPut = (req = request, res = response) => {};
+const profesoresPut = async (req = request, res = response) => {
+  const { id } = req.params;
+  const { body } = req;
 
-const profesoresDelete = (req = request, res = response) => {};
+  console.log(body);
+
+  try {
+    const profesor = await Profesor.findByPk(id);
+    if (!profesor)
+      res.status(404).json({ msg: `No existe un profesor con el id ${id}` });
+
+    await profesor.update(body);
+    // await profesor.save();
+
+    res.status(200).json(profesor);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hable con el administrador" });
+  }
+};
+
+const profesoresDelete = async (req = request, res = response) => {
+  const { id } = req.params;
+
+  const profesor = await Profesor.findByPk(id);
+  if (!profesor)
+    res.status(404).json({ msg: `No existe un profesor con el id ${id}` });
+
+  // await profesor?.destroy();
+  await profesor?.update({ estado: false });
+
+  res.json({ profesor });
+};
 
 module.exports = {
   profesoresGet,
+  profesoresGets,
   profesoresPost,
   profesoresPut,
   profesoresDelete,
--- backend/routes/profesores.js
+++ backend/routes/profesores.js
@@ -1,6 +1,23 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
+const {
+  profesoresGet,
+  profesoresPost,
+  profesoresPut,
+  profesoresDelete,
+  profesoresGets,
+} = require("../controllers/profesores");
 
 const router = Router();
+
+router.get("/", [], profesoresGet);
+
+router.get("/:id", [], profesoresGets);
+
+router.post("/", [], profesoresPost);
+
+router.put("/:id", [], profesoresPut);
+
+router.delete("/:id", [], profesoresDelete);
 
 module.exports = router;
