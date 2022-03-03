c2de71aba6754356c34f1093ba5c7760f576b70c
Carlos Villena <cosasdecardan@gmail.com>
2022-03-02 18:43:07 -0600

    adding controller for atomos

--- backend/controllers/atomos.js
+++ backend/controllers/atomos.js
@@ -1,14 +1,75 @@
-const { Router } = require("express");
-const { check } = require("express-validator");
+const { request, response } = require("express");
+const bcryptjs = require("bcryptjs");
 
-const router = Router();
+const { Atomo } = require("../models/atomos");
 
-// router.get();
+const atomosGet = async (req = request, res = response) => {
+  const atomos = await Atomo.findAll();
 
-// router.post();
+  res.status(201).json({ atomos });
+};
 
-// router.patch();
+const atomoGet = async (req = request, res = response) => {
+  const { id } = req.params;
+  const { body } = req;
 
-// router.delete();
+  const atomo = await Atomo.findByPk(id);
 
-module.exports = router;
+  if (atomo) res.json({ atomo });
+  else res.status(401).json({ msg: `No existe un atomo con el id ${id}` });
+};
+
+const atomoPost = async (req = request, res = response) => {
+  const { body } = req;
+  console.log(body);
+
+  try {
+    const atomo = await Atomo.build(body);
+
+    await atomo.save();
+
+    res.status(201).json({ atomo });
+  } catch (err) {}
+};
+
+const atomoPut = async (req = request, res = response) => {
+  const { id } = req.params;
+  const { body } = req;
+
+  console.log(body);
+
+  try {
+    const atomo = await Atomo.findByPk(id);
+    if (!atomo)
+      res.status(404).json({ msg: `No existe un atomo con el id ${id}` });
+
+    await atomo.update(body);
+    // await atomo.save();
+
+    res.status(200).json(atomo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Hable con el administrador" });
+  }
+};
+
+const atomoDelete = async (req = request, res = response) => {
+  const { id } = req.params;
+
+  const atomo = await Atomo.findByPk(id);
+  if (!atomo)
+    res.status(404).json({ msg: `No existe un atomo con el id ${id}` });
+
+  await atomo?.destroy();
+  // await atomo?.update({ estado: false });
+
+  res.json({ atomo });
+};
+
+module.exports = {
+  atomosGet,
+  atomoGet,
+  atomoPost,
+  atomoPut,
+  atomoDelete,
+};
--- backend/routes/atomos.js
+++ backend/routes/atomos.js
@@ -1,6 +1,23 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
+const {
+  atomosGet,
+  atomoGet,
+  atomoPost,
+  atomoPut,
+  atomoDelete,
+} = require("../controllers/atomos");
 
 const router = Router();
+
+router.get("/", [], atomosGet);
+
+router.get("/:id", [], atomoGet);
+
+router.post("/", [], atomoPost);
+
+router.put("/:id", [], atomoPut);
+
+router.delete("/:id", [], atomoDelete);
 
 module.exports = router;
