98b2202702da7c680c7ea32c160816c57dd18ed0
Cardan123 <cosasdecardan@gmail.com>
2022-03-28 21:53:12 -0600

    adding connection with create group and lab

--- frontend/src/components/new/NewGroupScreen.js
+++ frontend/src/components/new/NewGroupScreen.js
@@ -1,26 +1,89 @@
-import React from 'react'
-import newgroup from '../../assets/img/newgroup.jpg'
+import axios from "axios";
+import React, { useRef, useState } from "react";
+import newgroup from "../../assets/img/newgroup.jpg";
 export const NewGroupScreen = () => {
-    return (
-            <div className='config__container'>
-                <div className='config__image-container'>
-                    <div className='config__fondo'>
-                        <img src={newgroup}></img>
-                    </div>
-                </div>
-                <div className='config__text-container'>
-                    <div className='config__header'>
-                        <h3 className='config__subtitle'>Nuevo Grupo</h3>
-                    </div>
-                    <h2 className='config__title'></h2>
-                    <form>
-                        <label className='config__label'>Nombre</label>
-                        <input type="text" name='name' id='name' className='config__input'/>
-                        <label className='config__label'>Profesor</label>
-                        <input type="text" name='profesor' id='profesor' className='config__input'/>
-                        <button type="submit" className='config__button'>Crear</button>
-                    </form>
-                </div>
-            </div>
-    )
-}
+  const nameInputRef = useRef();
+  const idProfesorInputRef = useRef();
+
+  const [error, setError] = useState(false);
+
+  const insertGroup = (data) => {
+    let config = {
+      method: "post",
+      url: "http://127.0.0.1:8080/api/grupos",
+      headers: {},
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredIdProfesor = idProfesorInputRef.current.value;
+
+    if (enteredName.length === 0 || enteredIdProfesor.length === 0) {
+      setError(true);
+      return;
+    }
+
+    const group = {
+      nombre: enteredName,
+      numeroAlumnos: 0,
+      estado: true,
+      idProfesor: 1,
+    };
+
+    insertGroup(group);
+  };
+
+  return (
+    <div className="config__container">
+      <div className="config__image-container">
+        <div className="config__fondo">
+          <img src={newgroup}></img>
+        </div>
+      </div>
+      <div className="config__text-container">
+        <div className="config__header">
+          <h3 className="config__subtitle">Nuevo Grupo</h3>
+        </div>
+        <h2 className="config__title"></h2>
+        <form>
+          <label className="config__label">Nombre</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="config__input"
+            ref={nameInputRef}
+          />
+          <label className="config__label">Id Profesor</label>
+          <input
+            type="number"
+            name="IdProfesor"
+            id="IdProfesor"
+            className="config__input"
+            ref={idProfesorInputRef}
+          />
+          {error && <p>Revisa los campos del formulario</p>}
+          <button
+            type="submit"
+            className="config__button"
+            onClick={handleClick}
+          >
+            Crear
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
--- frontend/src/components/new/NewLabScreen.js
+++ frontend/src/components/new/NewLabScreen.js
@@ -1,28 +1,103 @@
-import React from 'react'
-import newlab from '../../assets/img/newlab.png'
+import axios from "axios";
+import React, { useState, useRef } from "react";
+import newlab from "../../assets/img/newlab.png";
 export const NewLabScreen = () => {
-    return (
-        <div className='config__container'>
-            <div className='config__image-container'>
-                <div className='config__fondo'>
-                    <img src={newlab}></img>
-                </div>
-            </div>
-            <div className='config__text-container'>
-                <div className='config__header'>
-                    <h3 className='config__subtitle'>Nuevo Laboratorio</h3>
-                </div>
-                <h2 className='config__title'></h2>
-                <form>
-                    <label className='config__label'>Nombre</label>
-                    <input type="text" name='name' id='name' className='config__input'/>
-                    <label className='config__label'>Profesor</label>
-                    <input type="text" name='profesor' id='profesor' className='config__input'/>
-                    <label className='config__label'>Breve descripción</label>
-                    <textarea name='descripcion' id='descripcion' className='config__input' rows="4" cols="50"/>
-                    <button type="submit" className='config__button'>Crear</button>
-                </form>
-            </div>
+  const nameInputRef = useRef();
+  const idGroupInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  const [error, setError] = useState(false);
+
+  const insertLab = (data) => {
+    let config = {
+      method: "post",
+      url: "http://127.0.0.1:8080/api/laboratorios",
+      headers: {},
+      data: data,
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const enteredGroup = idGroupInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    if (
+      enteredName.length === 0 ||
+      enteredGroup === 0 ||
+      descriptionInputRef === 0
+    ) {
+      setError(true);
+      return;
+    }
+
+    const laboratorio = {
+      nombre: enteredName,
+      descripcion: enteredDescription,
+      idGrupo: enteredGroup,
+    };
+
+    insertLab(laboratorio);
+  };
+
+  return (
+    <div className="config__container">
+      <div className="config__image-container">
+        <div className="config__fondo">
+          <img src={newlab}></img>
         </div>
-)
-}
+      </div>
+      <div className="config__text-container">
+        <div className="config__header">
+          <h3 className="config__subtitle">Nuevo Laboratorio</h3>
+        </div>
+        {/* <h2 className="config__title"></h2> */}
+        <form>
+          <label className="config__label">Nombre</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="config__input"
+            ref={nameInputRef}
+          />
+          <label className="config__label">Id Grupo</label>
+          <input
+            type="number"
+            name="grupo"
+            id="grupo"
+            className="config__input"
+            ref={idGroupInputRef}
+          />
+          <label className="config__label">Breve descripción</label>
+          <textarea
+            name="descripcion"
+            id="descripcion"
+            className="config__input"
+            rows="4"
+            cols="50"
+            ref={descriptionInputRef}
+          />
+          {error && <p>Revisa los campos del formulario</p>}
+          <button
+            type="submit"
+            className="config__button"
+            onClick={handleClick}
+          >
+            Crear
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
